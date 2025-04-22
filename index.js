a8f23df20d3550d1ba5b692196ea1e7f32d9c376
vaibhav kottari <kottarivaibhav@gmail.com>
2025-04-22 08:00:13 +0530

    backend changes

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173','https://book-store-frontend-phi-azure.vercel.app'],
+    credentials: true
+}))
+
+// routes
+const bookRoutes = require('./src/books/book.route');
+const orderRoutes = require('./src/orders/order.route');
+const userRoutes = require('./src/users/user.route');
+
+
+app.use("/api/books", bookRoutes)
+app.use("/api/prders",orderRoutes)
+app.use("api/auth",userRoutes);
+
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  app.use("/", (req, res) => {
+    res.send("Book Store Server is running!");
+  });
+}
+
+main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
